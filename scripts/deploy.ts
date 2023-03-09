b9845aa35eb3d166a71f64f2b6895a13dfe38835
Jungwoo Pyo <jungwoo_pyo@comcom.ai>
2023-03-09 16:42:05 +0900

    feat: deploy script impl.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,45 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+import { AINFT721Upgradeable__factory, AINFT721LogicV2__factory } from "../typechain-types";
+import { proxy } from "../typechain-types/@openzeppelin/contracts-upgradeable";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Deploy the proxy contract
+  const AINFT721Upgradeable: AINFT721Upgradeable__factory = await ethers.getContractFactory("AINFT721Upgradeable");
+  const proxyContract = await upgrades.deployProxy(
+    AINFT721Upgradeable,
+    ["Deploy AINFT for test", "AINFT"],
+    {
+      initializer: "initialize",
+      kind: 'uups',
+    });
+    
+  await proxyContract.deployed();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  console.log("Proxy Contract address:  ", proxyContract.address);
+  console.log("deployer:                ", await proxyContract.signer.getAddress())
+  console.log("Name:                    ", await proxyContract.name());
+  console.log("Symbol:                  ", await proxyContract.symbol());
+  console.log("logicVersion:            ", await proxyContract.logicVersion());
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // Upgrade logic contract
 
-  await lock.deployed();
+  const AINFT721LogicV2: AINFT721LogicV2__factory = await ethers.getContractFactory("AINFT721LogicV2");
+  const logicV2Contract = await upgrades.upgradeProxy(
+    proxyContract.address,
+    AINFT721LogicV2,
+    {
+      kind: 'uups',
+    }
+  )
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Logic Contract address:  ", logicV2Contract.address);
+  console.log("deployer:                ", await logicV2Contract.signer.getAddress());
+  console.log("logicVersion:            ", await proxyContract.logicVersion());
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
